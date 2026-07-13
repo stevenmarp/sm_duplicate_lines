5fc68a3a160f680f3dda508020b0ebf6452f4a61
stevenmarp <100309799+stevenmarp@users.noreply.github.com>
2026-07-13 11:02:47 +0700

    update fields_patch for robustness

--- sm_duplicate_lines/models/fields_patch.py
+++ sm_duplicate_lines/models/fields_patch.py
@@ -3,9 +3,12 @@ from odoo.fields import Field
 _original_get_description = Field.get_description
 
 
-def _patched_get_description(self, env, attributes=None):
-    desc = _original_get_description(self, env, attributes)
-    if attributes and 'copy' in attributes and 'copy' not in desc:
+def _patched_get_description(self, env, *args, **kwargs):
+    desc = _original_get_description(self, env, *args, **kwargs)
+    attributes = kwargs.get('attributes')
+    if not attributes and len(args) > 0:
+        attributes = args[0]
+    if attributes is None or 'copy' in attributes:
         desc['copy'] = self.copy
     return desc
 
